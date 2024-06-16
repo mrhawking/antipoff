3e3583e5f718b4a861392eb4e7c64fdd609d755a
Nata Tsvetkova <nvtsvetkova92@mail.ru>
2024-06-17 02:52:43 +0300

    deleted unused files

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { Provider } from 'react-redux'
-import { store } from './store/index.ts'
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import { Provider } from 'react-redux';
+import { store } from './store/index.ts';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <Provider store={store}>
